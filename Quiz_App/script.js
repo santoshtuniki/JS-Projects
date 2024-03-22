5682a26068ece7ca11198586c6c540335b6e7319
Saisantoshkumar Tuniki <saisantosh.tuniki@onpassive.com>
2024-03-22 10:08:07 +0530

    Quiz App done

--- Quiz_App/script.js
+++ Quiz_App/script.js
@@ -25,6 +25,8 @@ const showQuestion = () => {
         button.innerHTML = answer.text;
         button.classList.add('btn');
         answerButtons.appendChild(button);
+        button.dataset.correct = answer.correct;
+        button.addEventListener('click', selectAnswer);
     })
 };
 
@@ -33,6 +35,50 @@ const resetState = () => {
     while (answerButtons.firstChild) {
         answerButtons.removeChild(answerButtons.firstChild)
     }
-}
+};
+
+const selectAnswer = (e) => {
+    const selectedBtn = e.target;
+    const isCorrect = selectedBtn.dataset.correct === 'true';
+    if (isCorrect) {
+        selectedBtn.classList.add('correct');
+        score++;
+    } else {
+        selectedBtn.classList.add('incorrect');
+    }
+
+    Array.from(answerButtons.children).forEach((button) => {
+        if (button.dataset.correct === 'true') {
+            button.classList.add('correct');
+        }
+        button.disabled = true;
+    });
+    nextButton.style.display = 'block';
+};
+
+const showScore = () => {
+    resetState();
+    questionElement.innerHTML = `You scored ${score} out of ${questions.length}!`;
+    nextButton.innerHTML = 'Play Again';
+    nextButton.style.display = 'block';
+};
+
+const handleNextButton = () => {
+    currentIndex++;
+    if (currentIndex < questions.length) {
+        showQuestion();
+    }else{
+        showScore();
+    }
+};
+
+
+nextButton.addEventListener('click', (e) => {
+    if (currentIndex < questions.length) {
+        handleNextButton();
+    } else {
+        startQuiz();
+    }
+})
 
 startQuiz();
